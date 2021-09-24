a9e2d9673604dbc4e145b5765cc6af4e7bffe25e
rahmadnet <rahmadnasution2@gmail.com>
2021-09-24 08:19:19 +0700

    seccond commit

--- app.js
+++ app.js
@@ -39,18 +39,6 @@ app.use(
     );
     app.use(cors());
 
-    // Request Config
-// app.use((req,res,next)=>{
-
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header("Access-Control-Allow-Headers",
-//     "Origin,X-Requested-With,Content-Type,Accept,Authorization");
-//     if (req.method==='OPTIONS'){
-//         res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
-//         return res.status(200).json({});
-//     }
-// next();
-// });
 
 // Api
 app.use('/jabatan', jabatanRouter);
